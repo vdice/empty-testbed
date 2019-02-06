5d10e125961222d8c6ab60ed0e528788b4677228
Vaughn Dice <vadice@microsoft.com>
2019-02-06 13:33:55 -0700

    feat(brigade.js): add Checks event handlers

--- brigade.js
+++ brigade.js
@@ -1,11 +1,16 @@
 const { events, Job}  = require("brigadier");
 const { Helper } = require("/vcs/helpers");
 
-events.on("exec", (e, project) => {
+function echoAndNotify(e, project) {
   try {
     var helper = new Helper(e, project, Job);
-    helper.slackNotify("===> exec event received!");
+    helper.slackNotify(`===> ${e.type} event received!`);
   } catch (e) {
     console.log(e);
   }
-})
+}
+
+events.on("exec", echoAndNotify);
+events.on("check_suite:requested", echoAndNotify);
+events.on("check_suite:rerequested", echoAndNotify);
+events.on("check_run:rerequested", echoAndNotify);
